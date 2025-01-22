0e1749a9bff88c759782ab6bf1962ddd99f61574
Grover Chiu <48580749+crusade1275@users.noreply.github.com>
2025-01-22 15:13:42 +0800

    Finished task 3.

--- week3/app/task3.js
+++ week3/app/task3.js
@@ -1,3 +1,5 @@
+// 暫時 comment 導覽列
+
 let menuList = document.getElementById("menuList");
 
 function toggleMenu() {
@@ -15,3 +17,48 @@ function closeMenu() {
     menuList.style.display = "block";
   }
 }
+
+let fetchPromise = fetch(
+  "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
+);
+
+let getSpotList = async (url) => {
+  try {
+    let response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    let JSON = await response.json();
+    let spot_list = JSON.data.results;
+    let figures = document.querySelectorAll("figure");
+    let text_item = document.querySelectorAll("div.text");
+
+    for (i = 0; i < figures.length && i < spot_list.length; i++) {
+      const figure = figures[i];
+      let img = "https://" + spot_list[i].filelist.split("https://")[1];
+      figure.style.backgroundImage = `url(${img})`;
+    }
+
+    for (i = 0; i < text_item.length && i < spot_list.length; i++) {
+      let div_text = text_item[i];
+      var p_tag = document.createElement("p");
+      let title = spot_list[i].stitle;
+      p_tag.textContent = `${title}`;
+      div_text.appendChild(p_tag);
+    }
+  } catch (e) {
+    console.error("There was a problem with the fetch operation:", e);
+  }
+};
+
+getSpotList(
+  "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
+);
+
+// 監聽窗口大小變更
+// window.addEventListener("resize", () => {
+//   getSpotList(
+//     "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
+//   );
+// });
