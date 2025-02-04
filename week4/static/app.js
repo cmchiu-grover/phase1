bc168c27d150858dc3625a6884392f435aa3dc36
Grover Chiu <48580749+crusade1275@users.noreply.github.com>
2025-02-04 17:01:50 +0800

    Modified JavaScript.

--- week4/static/app.js
+++ week4/static/app.js
@@ -1,15 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-  if (form) {
-    form.addEventListener("submit", function (event) {
-      const checkbox = document.getElementById("agree");
-      if (!checkbox.checked) {
-        alert("請先勾選同意條款。");
-        event.preventDefault();
-      }
-    });
+function agreeCheck(event) {
+  const checkbox = document.getElementById("agree");
+
+  if (!checkbox.checked) {
+    alert("請先勾選同意條款。");
+    event.preventDefault();
   }
-});
+}
 
 function numConfirm(event) {
   event.preventDefault();
